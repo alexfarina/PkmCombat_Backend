00fe15e144c27e2f323e65a6750a4d2cf84fda80
alexfarina <afarinac23@fpcoruna.afundacion.org>
2026-04-02 00:06:21 +0200

    issue1, user registration

--- PkmCombat_backend/rest_api/admin.py
+++ PkmCombat_backend/rest_api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, PkmMoves, PkmStats, Pokemon, Moves , Battle, Team
+
+admin.site.register(Team)
+admin.site.register(User)
+admin.site.register(PkmStats)
+admin.site.register(PkmMoves)
+admin.site.register(Pokemon)
+admin.site.register(Moves)
+admin.site.register(Battle)
--- PkmCombat_backend/rest_api/views.py
+++ PkmCombat_backend/rest_api/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+import secrets
+import bcrypt
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_api.models import User,Team,Battle,Moves,PkmMoves,PkmStats,Pokemon
+
+
+@csrf_exempt
+def register(request):
+    if request.method!="POST":
+        return JsonResponse({"error": "HTTP method unsupported"},status=405)
+    try:
+        body_json=json.loads(request.body)
+        json_name=body_json["name"]
+        json_email=body_json["email"]
+        json_password=body_json["password"]
+    except (json.JSONDecodeError, KeyError):
+        return JsonResponse({"error":"Missing parametres in body request"},status=400)
+
+    if len(json_name)<3:
+        return JsonResponse({"error": "The name is too short"}, status=400)
+    if len(json_email)<6:
+        return JsonResponse({"error": "The email is too short"}, status=400)
+    if len(json_password)<6:
+        return JsonResponse({"error": "The password is too short"}, status=400)
+    if "@" not in json_email:
+        return JsonResponse({"error": "Invalid json email"}, status=400)
+    if User.objects.filter(name=json_name).exists():
+        return JsonResponse({"error": "This username already exists"}, status=409)
+    if User.objects.filter(email=json_email).exists():
+        return JsonResponse({"error": "This email already exists"}, status=409)
+
+    salted_and_hashed_pass = bcrypt.hashpw(
+        json_password.encode("utf8"),
+        bcrypt.gensalt()
+    ).decode("utf8")
+    radom_token= secrets.token_hex(10)
+
+    User.objects.create(name=json_name, email= json_email, encrypted_pass=salted_and_hashed_pass)
+
+    return JsonResponse({"registered": True, "token": radom_token}, status=201)
